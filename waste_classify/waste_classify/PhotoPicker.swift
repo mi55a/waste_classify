8cb162a0ad68193e84219ebbf25b06d7a2e539c8
mi55a <milymendez678@gmail.com>
2024-12-06 01:13:49 -0600

    added image picker and ml model

--- waste_classify/waste_classify/PhotoPicker.swift
+++ waste_classify/waste_classify/PhotoPicker.swift
@@ -7,7 +7,8 @@
 
 import SwiftUI
 import PhotosUI
-
+import CoreML
+/*
 @MainActor
 final class PhotoPickerViewModel: ObservableObject {
     @Published private(set) var selectedImage: UIImage? = nil
@@ -29,25 +30,80 @@ final class PhotoPickerViewModel: ObservableObject {
     }
     
 }
+*/
+
 
 struct PhotoPicker: View {
-    @StateObject private var viewModel = PhotoPickerViewModel()
+    let photos = ["Metal", "Paper", "Water_Bottle", "Waste", "Cardboard"]
+    @State private var currentIndex: Int = 0;
+    let model = WasteClassifierML()
+    @State private var classificationLabel: String = ""
+
+   
     var body: some View {
-        VStack(spacing:40) {
-            if let image = viewModel.selectedImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width:200, height:200)
-                    .cornerRadius(20)
+        VStack {
+            /*
+             @StateObject private var viewModel = PhotoPickerViewModel()
+             if let image = viewModel.selectedImage {
+             Image(uiImage: image)
+             .resizable()
+             .scaledToFill()
+             .frame(width:200, height:200)
+             .cornerRadius(20)
+             
+             }
+             */
+            Image(photos[currentIndex])
+                .resizable()
+                .frame(width:200, height:200)
+            VStack {
+                HStack {
+                    Button("Back") {
+                        if self.currentIndex >= self.photos.count {
+                            self.currentIndex = self.currentIndex - 1
+                        } else {
+                            self.currentIndex = 0
+                        }
+                    }
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .background(Color.blue)
                     
-            }
-            PhotosPicker(selection: $viewModel.imageSelection) {
-                Text("Pick your photo to classify")
-                    .foregroundColor(.blue)
+                    Button("Next") {
+                        if self.currentIndex < self.photos.count - 1 {
+                            self.currentIndex = self.currentIndex + 1
+                        } else {
+                            self.currentIndex = 0
+                        }
+                    }
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .background(Color.pink)
+                    Button("Classify") {
+                        // Code later
+                    }
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .background(Color.green)
+                    
+                }
                 
+                Text("Classification result: ")
+                    .padding()
+                    .font(.body)
+                
+                /*
+                 PhotosPicker(selection: $viewModel.imageSelection) {
+                 Text("Pick your photo to classify")
+                 .foregroundColor(.blue)
+                 }
+                 */
             }
+            Spacer()
         }
+    }
+    private func classifyImage() {
+        // Almost there
     }
 }
 
